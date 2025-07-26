7bf4b3259a4fca40a6a395f9cbda4c7748610f30
dangkhoa <dodangkhoaa1@gmail.com>
2025-07-26 13:45:25 +0700

    Modified: Select only what the screen show first 11 queries

--- TrelloQueries/TrelloQueryByScreen.sql
+++ TrelloQueries/TrelloQueryByScreen.sql
@@ -1,60 +1,125 @@
 ﻿--1. Home Page on the Boards tab → Recently viewed section, list all boards that the user has accessed recently.
-SELECT bu.UserId, bo.[Name] AS board_name
+SELECT 
+        bo.[Name] AS board_name, 
+        bo.BackgroundUrl
 FROM BoardUsers bu
 JOIN Boards bo ON bo.Id = bu.BoardId
 WHERE UserId = 1
 ORDER BY bu.AccessedAt DESC
 
 --2. Home Page on the Boards tab → Your Workspaces section, list all workspaces that the current user is a member of.
-SELECT *
+SELECT 
+    wo.Name, 
+    wo.LogoUrl
 FROM Workspaces wo
 JOIN Members me ON me.OwnerId = wo.Id
 WHERE me.OwnerTypeId = 1 AND me.UserId = 1
 
 --3. Home Page on the Boards tab → Workspace item → Boards button, list all boards  that the current user is a member of belonging to a specific workspace.
-SELECT *
+SELECT 
+    bo.[Name] AS board_name, 
+    bo.BackgroundUrl
 FROM Boards bo
 JOIN Members me ON me.OwnerId = bo.Id
-WHERE bo.WorkspaceId = 1 and me.UserId = 1 AND me.OwnerTypeId = 2
+WHERE bo.WorkspaceId = 1 
+    AND me.UserId = 1 
+    AND me.OwnerTypeId = 2
 
 --4. Home Page on the Header (top right corner), query boards have name that contains the keyword 'ab'
-SELECT * FROM Boards WHERE [Name] LIKE '%ab%'
+SELECT 
+    bo.[Name], 
+    wo.[Name], 
+    bo.[Status]
+FROM Boards bo
+JOIN Workspaces wo ON wo.Id = bo.Id
+WHERE bo.[Name] LIKE '%ab%'
 
 --5. Home Page on the Header (top right corner), show the total number of unread notifications of the user.
-SELECT COUNT(*) AS number_of_notifications
+SELECT 
+    COUNT(ac.UserId) AS number_of_notifications
 FROM Notifications [no]
 JOIN Activities ac ON ac.Id = no.ActivityId
 WHERE ac.UserId = 2 AND no.[Status] = 'UNREAD'
 
 --6. Home Page on the Templates tab → Main area, list all available public or user-created templates.
-SELECT * 
-FROM Templates
+SELECT 
+    us.PictureUrl AS user_picture, 
+    us.Username AS author, 
+    bo.BackgroundUrl AS board_background, 
+    te.Title AS template_title, 
+    te.[Description] AS tempalte_description, 
+    te.Viewed, 
+    te.Copied   
+FROM Templates te
+JOIN Users us ON us.Id = te.CreatedBy
+JOIn Boards bo ON bo.Id = te.BoardId
 ORDER BY Viewed DESC, Copied DESC
 
 --7. Home Page on the Templates tab → Sidebar, list all template categories available for filtering.
-SELECT *
+SELECT 
+    IconUrl AS template_category_icon, 
+    [Name] AS template_name
 FROM TemplateCategories
 
 --8. Home Page on the Templates tab, query templates have title that contains the keyword 'da' 
-SELECT * FROM Templates WHERE Title LIKE '%da%'
+SELECT 
+    bo.BackgroundUrl AS board_background, 
+    us.Username AS created_by, 
+    te.Title AS template_title
+FROM Templates te
+JOIN Users us ON us.Id = te.CreatedBy
+JOIN Boards bo ON bo.Id = te.BoardId
+WHERE Title LIKE '%da%'
 
 --9. Home Page on the Home tab → Checklist section, list all checklist items assigned to the user with status set to false (incomplete).
-SELECT * 
+SELECT 
+    cli.[Name] AS checklist_item_name, 
+    cli.[Status] AS checklist_item_status,
+    ca.Title AS card_title, 
+    bo.[Name] AS board_name,
+    us.PictureUrl
 FROM CheckListItems cli
+JOIN CheckLists cl ON cl.Id = cli.CheckListId
+JOIN Cards ca ON ca.Id = cl.CardId
+JOIN Stages st ON st.Id = ca.StageId
+JOIN Boards bo ON bo.Id = st.BoardId
 JOIN Members me ON me.Id = cli.MemberId
+JOIN Users us ON us.Id = me.UserId
 WHERE cli.[Status] = 0 AND me.UserId = 1
 
 --10. Home Page on the Home tab → Assigned cards section, list all cards that are currently assigned to the user.
-SELECT *
-FROM Cards ca
-JOIN Members me ON me.OwnerId = ca.Id
-WHERE me.OwnerTypeId = 3 AND me.UserId = 1 
-
---11. Home Page on the Home tab → Activity feed section, list all recent activities performed by the user.
-SELECT *
-FROM Activities
-WHERE UserId = 1
-Order By CreatedAt DESC
+SELECT 
+    ca.Title AS card_title,
+    bo.[Name] AS board_name,
+    st.Title AS stage_title,
+    us.PictureUrl AS user_picture,
+    ABS(DATEDIFF(DAY, GETDATE(), me.JoinedAt)) AS day_ago
+FROM (Cards ca 
+    JOIN Members me ON me.OwnerId = ca.Id 
+                        AND me.UserId = 1 
+                        AND me.OwnerTypeId = 3)
+JOIN Stages st ON st.Id = ca.StageId
+JOIN Boards bo ON bo.Id = st.BoardId
+JOIN Users us ON us.Id = me.UserId
+ORDER BY day_ago
+--11. Home Page on the Home tab → Activity feed section, list all recent card's activities in the user's card.
+SELECT 
+    ca.Title AS card_title,
+    wo.[Name] AS workspace_name,
+    bo.[Name] AS board_name,
+    st.Title AS stage_title,
+    us.Username AS username,
+    us.PictureUrl AS user_picture,
+    ABS(DATEDIFF(DAY, GETDATE(), ac.CreatedAt)) AS day_ago,
+    ac.[Description] AS activity_description
+FROM (SELECT UserId, OwnerTypeId, OwnerId, [Description], CreatedAt
+    FROM Activities WHERE OwnerTypeId = 3) ac
+JOIN Cards ca ON ca.Id = ac.OwnerId AND ca.CreatedBy = 1
+JOIN Stages st ON st.Id = ca.StageId
+JOIN Boards bo ON bo.Id = st.BoardId
+JOIN Workspaces wo ON wo.Id = bo.WorkspaceId
+JOIN Users us ON us.Id = ac.UserId
+Order By day_ago
 
 --12. Home Page on the Workspace page → Boards section, list all boards under the selected workspace.
 SELECT *
@@ -102,5 +167,10 @@ WHERE bo.Id = 1
 --19. Board Page on the Share Board pop-up, list all permission options can choose
 SELECT *
 FROM [Permissions]
+
+--20 Board Page on the Setting pop-up, list all board's setting key and user's choice of a specific user
+SELECT sk.KeyName, COALESCE(sv.Value, sk.DefaultValue) as setting_value, OwnerId
+FROM SettingKeys sk
+LEFT JOIN SettingValues sv ON sv.SettingKeyId = sk.Id AND sk.OwnerTypeId = 2 AND sv.OwnerId = 1
 
 
