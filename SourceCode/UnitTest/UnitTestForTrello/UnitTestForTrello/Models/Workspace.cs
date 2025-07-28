cc617195c4d5c56763d0daeaa9610c82e22123af
dangkhoa <dodangkhoaa1@gmail.com>
2025-07-28 23:39:09 +0700

    add: create 2 unit test in workspace

---
+++ SourceCode/UnitTest/UnitTestForTrello/UnitTestForTrello/Models/Workspace.cs
@@ -0,0 +1,13 @@
+﻿
+namespace UnitTestForTrello.Models
+{
+    public class Workspace
+    {
+        public string Title { get; set; }
+        public Workspace(string title)
+        {
+            if (String.IsNullOrEmpty(title)) throw new ArgumentNullException("title");
+            Title = title;
+        }
+    }
+}
