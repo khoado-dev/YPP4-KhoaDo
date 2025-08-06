5c5a559b5060da79b7390fafd192202a000f0e8c
dangkhoa <dodangkhoaa1@gmail.com>
2025-08-06 10:19:00 +0700

    add: write query paging by row, pivot after paging by row

--- MsList/004_khoadd_queries_mslist.sql
+++ MsList/004_khoadd_queries_mslist.sql
@@ -46,3 +46,72 @@ SELECT *
 FROM TemplateSampleCell
 WHERE CONTAINS(CellValue, '"asset*" OR "row*"');
 
+-- Paging List in MSList by row(limit the number of display row)
+DECLARE @PageSize INT = 2;
+DECLARE @PageIndex INT = 2;
+
+WITH PagedRows AS (
+    SELECT 
+        tsr.Id AS template_row_id,
+        tsr.DisplayOrder,
+        ROW_NUMBER() OVER (ORDER BY tsr.DisplayOrder) AS RowNum
+    FROM TemplateSampleRow tsr
+    WHERE tsr.ListTemplateId = 1
+)
+SELECT 
+    tc.Id AS template_column_id,
+    tc.ColumnName,
+    pr.template_row_id,
+    pr.DisplayOrder,
+    tsc.CellValue
+FROM PagedRows pr
+CROSS JOIN TemplateColumn tc
+LEFT JOIN TemplateSampleCell tsc 
+    ON tsc.TemplateColumnId = tc.Id 
+    AND tsc.TemplateSampleRowId = pr.template_row_id
+WHERE tc.ListTemplateId = 1
+  AND pr.RowNum BETWEEN (@PageIndex - 1) * @PageSize + 1 AND @PageIndex * @PageSize
+ORDER BY pr.DisplayOrder;
+
+-- PITVOT
+DECLARE @columns NVARCHAR(1000); 
+SELECT @columns = STRING_AGG(QUOTENAME(ColumnName), ',') --create a string to store all column name
+FROM TemplateColumn
+WHERE ListTemplateId = 1;
+
+DECLARE @sql NVARCHAR(1000) = '
+WITH PagedRows AS (
+    SELECT 
+        tsr.Id AS template_row_id,
+        tsr.DisplayOrder,
+        ROW_NUMBER() OVER (ORDER BY tsr.DisplayOrder) AS RowNum
+    FROM TemplateSampleRow tsr
+    WHERE tsr.ListTemplateId = 1
+),
+Source AS (
+    SELECT 
+        tc.ColumnName,
+        pr.template_row_id,
+        pr.DisplayOrder,
+        tsc.CellValue
+    FROM PagedRows pr
+    CROSS JOIN TemplateColumn tc
+    LEFT JOIN TemplateSampleCell tsc 
+        ON tsc.TemplateColumnId = tc.Id 
+        AND tsc.TemplateSampleRowId = pr.template_row_id
+    WHERE tc.ListTemplateId = 1
+      AND pr.RowNum BETWEEN (@PageIndex - 1) * @PageSize + 1 AND @PageIndex * @PageSize
+)
+SELECT *
+FROM Source
+PIVOT (
+    MAX(CellValue)
+    FOR ColumnName IN (' + @columns + ')
+) AS Pivoted
+ORDER BY DisplayOrder;
+';
+
+EXEC sp_executesql @sql, N'@PageSize INT, @PageIndex INT', @PageSize = 2, @PageIndex = 2;
+-- Paging List in MSList by column(limit the number of display column)
+
+--CACHING
