3294ea605e3ef245553ff654ad4b70f9daf102d0
dangkhoa <dodangkhoaa1@gmail.com>
2025-08-12 15:27:50 +0700

    Modified: unit test with dapper and sqlite GetUserStarredBoard

--- SourceCode/UnitTest/UnitTestForTrello/UnitTestForTrello/Repositories/BoardRepository.cs
+++ SourceCode/UnitTest/UnitTestForTrello/UnitTestForTrello/Repositories/BoardRepository.cs
@@ -1,108 +1,46 @@
-﻿using System.Data.SqlClient;
-using UnitTestForTrello.Models;
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestForTrello.Models.DTOs;
 
 namespace UnitTestForTrello.Repositories
 {
-    public class BoardRepository : RepositoryBase
+    public class BoardRepository
     {
-        public BoardRepository(SqlConnection con, SqlTransaction tran) : base(con, tran) { }
+        private readonly IDbConnection _con;
+        private readonly IDbTransaction _tran;
 
-        public int Create(Board entity)
+        public BoardRepository(IDbConnection con, IDbTransaction tran)
         {
-            using var cmd = new SqlCommand(@"
-                    INSERT INTO Boards 
-                        (BoardName, BoardDescription, WorkspaceId, CreatedAt, CreatedBy, UpdatedAt, UpdatedBy, BackgroundUrl)
-                    VALUES 
-                        (@BoardName, @BoardDescription, @WorkspaceId, @CreatedAt, @CreatedBy, @UpdatedAt, @UpdatedBy, @BackgroundUrl);
-                    SELECT SCOPE_IDENTITY();", _con, _tran);
-
-            cmd.Parameters.AddWithValue("@BoardName", (object?)entity.BoardName ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@BoardDescription", (object?)entity.BoardDescription ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@WorkspaceId", (object?)entity.WorkspaceId ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@CreatedAt", (object?)entity.CreatedAt ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@CreatedBy", (object?)entity.CreatedBy ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@UpdatedAt", (object?)entity.UpdatedAt ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@UpdatedBy", (object?)entity.UpdatedBy ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@BackgroundUrl", (object?)entity.BackgroundUrl ?? DBNull.Value);
-
-            return Convert.ToInt32(cmd.ExecuteScalar());
+            _con = con;
+            _tran = tran;
         }
 
-        public Board? GetById(int id)
+        public IEnumerable<BoardDTO> GetStarredBoardsByUser(int userId)
         {
-            using var cmd = new SqlCommand("SELECT * FROM Boards WHERE Id = @Id", _con, _tran);
-            cmd.Parameters.AddWithValue("@Id", id);
+            const string sql = @"
+            SELECT
+                usb.UserId,
+                brd.Id AS BoardId,
+                brd.BackgroundUrl,
+                brd.BoardName,
+                brd.BoardStatus
+            FROM UserStarredBoard usb
+            JOIN Board brd ON brd.Id = usb.BoardId
+            WHERE usb.UserId = @UserId
+              AND brd.BoardStatus = 'active'
+            ORDER BY usb.CreatedAt DESC";
 
-            using var reader = cmd.ExecuteReader();
-            if (reader.Read())
-            {
-                return MapReaderToEntity(reader);
-            }
-            return null;
+            return _con.Query<BoardDTO>(sql, new { UserId = userId }, _tran);
         }
 
-        public List<Board> GetAll()
+        internal object GetStarredBoards(int loggedIngInUserId)
         {
-            var boards = new List<Board>();
-            using var cmd = new SqlCommand("SELECT * FROM Boards", _con, _tran);
-
-            using var reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                boards.Add(MapReaderToEntity(reader));
-            }
-            return boards;
-        }
-
-        public bool Update(Board entity)
-        {
-            using var cmd = new SqlCommand(@"
-                    UPDATE Boards SET
-                        BoardName = @BoardName,
-                        BoardDescription = @BoardDescription,
-                        WorkspaceId = @WorkspaceId,
-                        CreatedAt = @CreatedAt,
-                        CreatedBy = @CreatedBy,
-                        UpdatedAt = @UpdatedAt,
-                        UpdatedBy = @UpdatedBy,
-                        BackgroundUrl = @BackgroundUrl
-                    WHERE Id = @Id", _con, _tran);
-
-            cmd.Parameters.AddWithValue("@Id", entity.Id);
-            cmd.Parameters.AddWithValue("@BoardName", (object?)entity.BoardName ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@BoardDescription", (object?)entity.BoardDescription ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@WorkspaceId", (object?)entity.WorkspaceId ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@CreatedAt", (object?)entity.CreatedAt ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@CreatedBy", (object?)entity.CreatedBy ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@UpdatedAt", (object?)entity.UpdatedAt ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@UpdatedBy", (object?)entity.UpdatedBy ?? DBNull.Value);
-            cmd.Parameters.AddWithValue("@BackgroundUrl", (object?)entity.BackgroundUrl ?? DBNull.Value);
-
-            return cmd.ExecuteNonQuery() > 0;
-        }
-
-        public bool Delete(int id)
-        {
-            using var cmd = new SqlCommand("DELETE FROM Boards WHERE Id = @Id", _con, _tran);
-            cmd.Parameters.AddWithValue("@Id", id);
-
-            return cmd.ExecuteNonQuery() > 0;
-        }
-
-        protected Board MapReaderToEntity(SqlDataReader reader)
-        {
-            return new Board
-            {
-                Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                BoardName = reader["BoardName"] as string,
-                BoardDescription = reader["BoardDescription"] as string,
-                WorkspaceId = reader["WorkspaceId"] as int?,
-                CreatedAt = reader["CreatedAt"] as DateTime?,
-                CreatedBy = reader["CreatedBy"] as int?,
-                UpdatedAt = reader["UpdatedAt"] as DateTime?,
-                UpdatedBy = reader["UpdatedBy"] as int?,
-                BackgroundUrl = reader["BackgroundUrl"] as string
-            };
+            throw new NotImplementedException();
         }
     }
 }
---
+++ SourceCode/UnitTest/UnitTestForTrello/UnitTestForTrello/Services/BoardService.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using System.Data;
+using UnitTestForTrello.Models.DTOs;
+
+namespace UnitTestForTrello.Repositories
+{
+    public class BoardService
+    {
+        private readonly BoardRepository _boardRepository;
+
+        public BoardService(BoardRepository boardRepository)
+        {
+            _boardRepository = boardRepository;
+        }
+
+        public IEnumerable<BoardDTO> GetStarredBoards(int userId)
+        {
+            return _boardRepository.GetStarredBoardsByUser(userId);
+        }
+
+    }
+}
