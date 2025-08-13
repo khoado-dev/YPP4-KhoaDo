a48fc93477c049e877438e215645d0e7902443f9
dangkhoa <dodangkhoaa1@gmail.com>
2025-08-13 15:57:56 +0700

    add: create unit test for get membersby boardId

---
+++ SourceCode/UnitTest/UnitTestForTrello/UnitTestForTrello/Controllers/MemberController.cs
@@ -0,0 +1,18 @@
+﻿using UnitTestForTrello.Models.DTOs;
+using UnitTestForTrello.Services.IServices;
+
+namespace UnitTestForTrello.Controllers
+{
+    public class MemberController
+    {
+        private readonly IMemberService _memberService;
+        public MemberController(IMemberService memberService)
+        {
+            _memberService = memberService;
+        }
+        public IEnumerable<BoardMemberDTO> GetMembersByBoardId(int boardId)
+        {
+            return _memberService.GetMembersByBoardId(boardId);
+        }
+    }
+}
---
+++ SourceCode/UnitTest/UnitTestForTrello/UnitTestForTrello/Services/IServices/IMemberService.cs
@@ -0,0 +1,9 @@
+﻿using UnitTestForTrello.Models.DTOs;
+
+namespace UnitTestForTrello.Services.IServices
+{
+    public interface IMemberService
+    {
+        IEnumerable<BoardMemberDTO> GetMembersByBoardId(int boardId);
+    }
+}
