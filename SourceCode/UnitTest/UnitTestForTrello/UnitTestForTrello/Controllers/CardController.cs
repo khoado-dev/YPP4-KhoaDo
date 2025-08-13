4e294262810324aa8e4c7c57d12832072062d198
dangkhoa <dodangkhoaa1@gmail.com>
2025-08-13 14:38:57 +0700

    add: create unit test for get summary of cards by boardId

---
+++ SourceCode/UnitTest/UnitTestForTrello/UnitTestForTrello/Controllers/CardController.cs
@@ -0,0 +1,19 @@
+﻿using UnitTestForTrello.Models.DTOs;
+using UnitTestForTrello.Services.IServices;
+
+namespace UnitTestForTrello.Controllers
+{
+    public class CardController
+    {
+        private readonly ICardService _cardService;
+        public CardController(ICardService cardService)
+        {
+            _cardService = cardService;
+        }
+
+        public IEnumerable<CardSummaryDTO> GetCardSummariesByBoardId(int boardId)
+        {
+            return _cardService.GetCardSummariesByBoardId(boardId);
+        }
+    }
+}
