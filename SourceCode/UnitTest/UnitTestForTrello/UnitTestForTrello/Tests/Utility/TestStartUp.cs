16384c93f4619d716107b4e3e9bb263306328912
dangkhoa <dodangkhoaa1@gmail.com>
2025-08-18 15:52:35 +0700

    refactor(startup): extract DB init and module registrations

--- SourceCode/UnitTest/UnitTestForTrello/UnitTestForTrello/Tests/Utility/TestStartUp.cs
+++ SourceCode/UnitTest/UnitTestForTrello/UnitTestForTrello/Tests/Utility/TestStartUp.cs
@@ -15,7 +15,7 @@ namespace UnitTestForTrello;
 [TestClass]
 public static class TestStartUp
 {
-    private static ServiceCollection? _services;
+    private static IServiceCollection? _services;
     private static ServiceProvider? _root;
     private static SqliteConnection? _conn;
 
@@ -23,44 +23,31 @@ public static class TestStartUp
     [AssemblyInitialize]
     public static void AssemblyInit(TestContext context)
     {
-        _services = new ServiceCollection();
-
-        _services.AddSingleton<ICustomCache>(new CustomCache(TimeSpan.FromMinutes(1))); //Add custom cache with 1 minute Time To Live(TTL).
-
-        _conn = TestDatabaseHelper.GetInMemoryDatabaseConnection();
-        if (_conn?.State != ConnectionState.Open) _conn?.Open();
-        if (_conn is SqliteConnection) //Add No Lock for SQLite because SQLite does not support it in query.
-        {
-            _conn.Execute("PRAGMA journal_mode=WAL;"); //WAL helps readers not block writers and vice versa on file-backed DBs.
-            _conn.Execute("PRAGMA synchronous=NORMAL;"); // - synchronous=NORMAL balances durability vs. speed for tests.
-            _conn.Execute("PRAGMA busy_timeout = 5000;"); // - busy_timeout reduces SQLITE_BUSY errors when short locks happen.
-        }
-        _services.AddSingleton<IDbConnection>(_conn!);
-
-        TestDatabaseHelper.CreateInMemoryDatabaseAndSchema();
-        TestDatabaseHelper.SeedAllData();
-
-        _services.AddScoped<IBoardRepository, BoardRepository>();
-        _services.AddScoped<IBoardService, BoardService>();
-        _services.AddTransient<BoardController>();
-
-        _services.AddScoped<ICardRepository, CardRepository>();
-        _services.AddScoped<ICardService, CardService>();
-        _services.AddTransient<CardController>();
-
-        _services.AddScoped<IMemberRepository, MemberRepository>();
-        _services.AddScoped<IMemberService, MemberService>();
-        _services.AddTransient<MemberController>();
-
-        _services.AddScoped<IWorkspaceRepository, WorkspaceRepository>();
-        _services.AddScoped<IWorkspaceService, WorkspaceService>();
-        _services.AddTransient<WorkspaceController>();
-
-        _services.AddScoped<IUserRepository, UserRepository>();
-        _services.AddScoped<IUserService, UserService>();
-        _services.AddTransient<UserController>();
+        _services = new ServiceCollection()
+        .AddSingleton<ICustomCache>(new CustomCache(TimeSpan.FromMinutes(1)))
+        .AddSingleton<IDbConnection>(InitSqlite())
+        .AddBoardModule()
+        .AddCardModule()
+        .AddMemberModule()
+        .AddWorkspaceModule()
+        .AddUserModule();
 
         _root = new ServiceProvider(_services);
+    }
+
+    private static IDbConnection InitSqlite()
+    {
+        var conn = TestDatabaseHelper.GetInMemoryDatabaseConnection();
+        if (conn?.State != ConnectionState.Open) conn?.Open();
+        if (conn is SqliteConnection)
+        {
+            conn.Execute("PRAGMA journal_mode=WAL;");
+            conn.Execute("PRAGMA synchronous=NORMAL;");
+            conn.Execute("PRAGMA busy_timeout=5000;");
+        }
+        TestDatabaseHelper.CreateInMemoryDatabaseAndSchema();
+        TestDatabaseHelper.SeedAllData();
+        return conn!;
     }
     #endregion
 
