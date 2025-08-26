12ccb7ce9a763ced1f38db8e3569beb93cdf8fcf
dangkhoa <dodangkhoaa1@gmail.com>
2025-08-26 16:41:26 +0700

    add mvc: enable to render user profile view

---
+++ SourceCode/CustomMVC/CustomMVC/Mvc/Results/ViewResult.cs
@@ -0,0 +1,27 @@
+﻿using CustomMVC.Core.Http;
+using CustomMVC.Mvc.Views;
+
+namespace CustomMVC.Mvc.Results
+{
+    public sealed class ViewResult : IActionResult
+    {
+        private readonly string _viewName;
+        private readonly object? _model;
+
+        // Use SimpleViewEngine Default
+        private static readonly IViewEngine _engine = new SimpleViewEngine();
+
+        public ViewResult(string viewName, object? model)
+        {
+            _viewName = viewName;   // example "Users/Profile"
+            _model = model;
+        }
+
+        public async Task ExecuteAsync(HttpContext ctx)
+        {
+            var html = _engine.Render(_viewName, _model);
+            ctx.Response.StatusCode = 200;
+            await ctx.Response.WriteAsync(html, "text/html; charset=utf-8");
+        }
+    }
+}
---
+++ SourceCode/CustomMVC/CustomMVC/Mvc/Views/IViewEngine.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomMVC.Mvc.Views
+{
+    public interface IViewEngine
+    {
+        string Render(string viewName, object? model);
+    }
+}
